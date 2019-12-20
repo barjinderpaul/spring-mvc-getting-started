2ac802bcd94c4295017c0eafd41ddc05c39781b5
barjinderpaul <barjinderpaul.singh@gmail.com>
2019-12-20 17:36:36 +0530

    used @RequestParam annotation

--- src/main/java/com/hellospring/controllers/AddController.java
+++ src/main/java/com/hellospring/controllers/AddController.java
@@ -4,6 +4,7 @@ import com.hellospring.service.AddService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,9 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 public class AddController {
     @RequestMapping("/add")
-    public ModelAndView add(HttpServletRequest request, HttpServletResponse response) {
-        int firstNumber = Integer.parseInt(request.getParameter("firstNumber"));
-        int secondNumber = Integer.parseInt(request.getParameter("secondNumber"));
+    public ModelAndView add(@RequestParam("firstNumber") int firstNumber, @RequestParam("secondNumber") int secondNumber) {
 
         int result = AddService.add(firstNumber,secondNumber);
 
