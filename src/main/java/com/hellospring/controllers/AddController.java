55753db7db392fc40b3313ebe758759ce03a1e20
barjinderpaul <barjinderpaul.singh@gmail.com>
2019-12-20 17:13:16 +0530

    Implementing separation of concerns

--- src/main/java/com/hellospring/controllers/AddController.java
+++ src/main/java/com/hellospring/controllers/AddController.java
@@ -1,5 +1,6 @@
-package com.hellospring;
+package com.hellospring.controllers;
 
+import com.hellospring.service.AddService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,8 @@ public class AddController {
     public ModelAndView add(HttpServletRequest request, HttpServletResponse response) {
         int firstNumber = Integer.parseInt(request.getParameter("firstNumber"));
         int secondNumber = Integer.parseInt(request.getParameter("secondNumber"));
-        int result = firstNumber + secondNumber;
+
+        int result = AddService.add(firstNumber,secondNumber);
 
         ModelAndView mv = new ModelAndView();
         mv.setViewName("result.jsp");
---
+++ src/main/java/com/hellospring/service/AddService.java
@@ -0,0 +1,6 @@
+package com.hellospring.service;
+public class AddService {
+    public static int add(int firstNumber, int secondNumber ) {
+        return firstNumber + secondNumber;
+    }
+}
